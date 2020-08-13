621d9d72f94c8285a688c19370604d00328b73d0
Lucas Silveira <lugano1995@gmail.com>
2020-08-13 20:40:30 -0300

    inside array

---
+++ Array/insidearray/insidearray.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// demonstrando que os slices apontam para o mesmo array
+	s1 := make([]int, 10, 20)
+	s2 := append(s1, 1, 2, 3)
+
+	fmt.Println(s1, s2) // [0 0 0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0 0 0 1 2 3]
+
+	s1[0] = 7
+
+	fmt.Println(s1, s2) // [7 0 0 0 0 0 0 0 0 0] [7 0 0 0 0 0 0 0 0 0 1 2 3]
+
+}
