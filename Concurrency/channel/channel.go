bc27ca128061e474dc119461b02cd8cd1eadd495
Lucas Silveira <lugano1995@gmail.com>
2020-08-26 21:38:16 -0300

    concurrency channel

---
+++ Concurrency/channel/channel.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	ch := make(chan int, 1) // criando um channel de inteiros com um buffer contendo 1
+
+	ch <- 1 // enviando para o canal o valor 1 (escrita)
+
+	<-ch // recebendo o valor do canal (leitura) e descartando a mensagem
+
+	ch <- 2
+	fmt.Println(<-ch) // 2
+}
