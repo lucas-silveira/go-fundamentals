2742c4e54b0de8b6e29ea146fb0fb9103b772788
Lucas Silveira <lugano1995@gmail.com>
2021-02-01 09:50:56 -0300

    refactor: array module

--- Array/forrange/forrange.go
+++ Array/forrange/forrange.go
@@ -6,7 +6,7 @@ func main() {
 	numbers := [...]int{1, 2, 3, 4, 5}
 
 	for i, number := range numbers {
-		fmt.Printf("%d)  %d\n", i, number)
+		fmt.Printf("%d) %d\n", i, number)
 	}
 
 	for _, number := range numbers {
